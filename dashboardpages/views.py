9ff7de66a8718dd8678ba87d254bd2499cbf6fd0
audreyseager <audreyjy@byu.edu>
2022-10-25 22:55:14 -0600

    first commit

---
+++ dashboardpages/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def indexPageView(request) :
+    return render(request, 'dashboardpages/index.html')
+# Create your views here.
