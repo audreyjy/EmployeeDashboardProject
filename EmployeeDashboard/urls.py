8d425e28a71754c05513db6f57a4986b3537a4a9
camrynwhisenant <cwhis99@byu.edu>
2022-10-27 19:28:06 -0600

    tableau dashboard

--- EmployeeDashboard/urls.py
+++ EmployeeDashboard/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from dashboardpages.views import TableauPageView
 
 urlpatterns = [
     path("", include("dashboardpages.urls")), 
     path('admin/', admin.site.urls),
+    path("tableau/", TableauPageView, name="tableau"),
 ]
--- dashboardpages/views.py
+++ dashboardpages/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 def indexPageView(request) :
     return render(request, 'dashboardpages/index.html')
 # Create your views here.
+
+def TableauPageView(request) :
+    return render(request, 'dashboardpages/tableau.html')
