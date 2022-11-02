7923ca2973933db7b3ef6bcf7c8bf848b735f7c1
ytagrii <72105289+ytagrii@users.noreply.github.com>
2022-11-02 10:24:24 -0600

    more updates

--- dashboardpages/views.py
+++ dashboardpages/views.py
@@ -2,13 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Course, Job, Payment, Employee
 def indexPageView(request) :
-    ee = Employee.objects.all()
     job = Job.objects.all()
-    # spec = Specialty.objects.all()
     context ={
-        "ee": ee,
         "job": job,
-        # "spec": spec
     }
     return render(request, 'dashboardpages/index.html', context)
 # Create your views here.
